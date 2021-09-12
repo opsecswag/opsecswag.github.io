4637a8ba897b2a521565544c9df115e8281b297d
Dennis Reimann <mail@dennisreimann.de>
2021-09-12 16:22:19 +0200

    Shuffle products
    Closes #1

--- _assets/js/_inits.js
+++ _assets/js/_inits.js
@@ -6,7 +6,7 @@ import AOS from 'aos';
 import './vendor/_transition.js';
 import './vendor/_zoom.js';
 import InfiniteScroll from './components/_infiniteScroll.js';
-import { miscFlexVid, miscZoom, miscSocialShare } from './components/_miscellaneous.js';
+import { miscFlexVid, miscZoom, miscSocialShare, shuffle } from './components/_miscellaneous.js';
 import PageTransition from './components/_pageTransition.js';
 import Popup from './components/_popup.js';
 
@@ -22,6 +22,13 @@ $(() => {
     once: true
   });
   PageTransition.init();
+
+  const $postList = $('.posts-full')
+  if ($postList.length) {
+    const $posts = $('.posts__post');
+    $postList.empty();
+    shuffle($posts).each((i, $post) => $postList.append($post))
+  }
 
   if ($('.posts').length && $('.posts__next').length) {
     InfiniteScroll.init();
--- _assets/js/components/_miscellaneous.js
+++ _assets/js/components/_miscellaneous.js
@@ -16,9 +16,18 @@ const miscZoom = () => {
   });
 };
 
+const shuffle = arr => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+  };
+  return arr
+};
+
 // ----------------------------------------------
 // Exports
 // ----------------------------------------------
 module.exports = {
-  miscZoom
+  miscZoom,
+  shuffle
 };
